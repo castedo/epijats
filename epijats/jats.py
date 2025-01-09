1ce383a1c153af250fff092050eda04bb2dbc12b
Castedo Ellerman <castedo@castedo.com>
2025-01-09 22:12:56 +0000

    use Baseprint DOM to gen webstract authors

--- epijats/jats.py
+++ epijats/jats.py
@@ -3,34 +3,8 @@ from pathlib import Path
 from importlib import resources
 from typing import Any, Iterable
 
-from lxml import etree
-
+from .baseprint import BaseprintParse
 from .webstract import Webstract, Source
-
-
-def extract_nodes(tree: etree._Element, nodename: str) -> list[Any]:
-    return [node for node in tree.iterdescendants() if node.tag == nodename]
-
-
-def parse_authors(jats_src: Path) -> list[dict[str, Any]]:
-    with open(jats_src, "rb") as f:
-        tree = etree.parse(f).getroot()
-    article_meta_tag = extract_nodes(tree, "article-meta")[0]
-    ret = []
-    for tag in extract_nodes(article_meta_tag, "contrib"):
-        contributor: dict[str, Any] = {}
-        contributor["type"] = tag.attrib["contrib-type"]
-        assert "type" in contributor
-        contrib_id_tag = extract_nodes(tag, "contrib-id")[0]
-        assert "contrib-id-type" in contrib_id_tag.attrib
-        assert contrib_id_tag.attrib["contrib-id-type"] == "orcid"
-        contributor["orcid"] = contrib_id_tag.text
-        for email_tag in extract_nodes(tag, "email"):
-            contributor["email"] = [email_tag.text]
-        contributor["surname"] = extract_nodes(tag, "surname")[0].text
-        contributor["given-names"] = extract_nodes(tag, "given-names")[0].text
-        ret.append(contributor)
-    return ret
 
 
 def run_pandoc(args: Iterable[Any], echo: bool = True) -> bytes:
@@ -62,9 +36,19 @@ def webstract_from_jats(src: Path | str) -> Webstract:
         raise ValueError("JSOML webstract must be object/dictionary.")
     ret = Webstract(data)
     ret['source'] = Source(path=src)
-    ret['contributors'] = parse_authors(jats_src)
-    for c in ret['contributors']:
-        if 'orcid' in c:
-            c['orcid'] = c['orcid'].rsplit("/", 1)[-1]
+    parse = BaseprintParse()
+    bp = parse.baseprint(jats_src)
+    if bp is None:
+        raise ValueError()
+    ret['contributors'] = list()
+    for a in bp.authors:
+        d = {'surname': a.surname, 'type': 'author'}
+        if a.given_names:
+            d['given-names'] = a.given_names
+        if a.email:
+            d['email'] = [a.email]
+        if a.orcid:
+            d['orcid'] = a.orcid.as_19chars()
+        ret['contributors'].append(d)
 
     return ret
