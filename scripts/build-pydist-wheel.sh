40f63f4dc8827373338a4e1a86a645fa85a16752
Castedo Ellerman <castedo@castedo.com>
2025-03-27 21:05:38 +0000

    keep version in wheel filename

--- scripts/build-pydist-wheel.sh
+++ scripts/build-pydist-wheel.sh
@@ -1,9 +1,10 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 set -o errexit -o pipefail -o nounset
 
 set -o xtrace
 
-VERSION=$(python -m setuptools_scm --root $PYROOT)
 python3 -m build --wheel --no-isolation --outdir $DEST $PYROOT
+VERSION=$(python3 -m setuptools_scm --root $PYROOT)
+CLEAN_VERSION=$(python3 -m setuptools_scm --root $PYROOT --strip-dev)
 cd $DEST
-mv epijats-$VERSION-py3-none-any.whl epijats-py3-none-any.whl
+mv epijats-$VERSION-py3-none-any.whl epijats-$CLEAN_VERSION-py3-none-any.whl
